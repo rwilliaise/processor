958b74418516048b8f824ed9b0a9e6f4fb2f16cb
lego man <rajawilliams24@gmail.com>
2020-07-03 22:32:09 -0700

    Add more ram

--- MainProcess.lua
+++ MainProcess.lua
@@ -1,9 +1,12 @@
 local Memory = require("memory/Memory")
+local Parser = require("opcodes/Parser")
 
 local file_ROM = arg[1] or "rom.bin"
 
 local ROM = Memory.FromFile(file_ROM, true)
-local RAM = Memory.new(1024) -- we may need more addresses eventually
+local RAM = Memory.new(65535) -- we may need more addresses eventually
 local Registers = Memory.new(7)
+
+
 
 -- TODO: opcodes
--- opcodes/Parser.lua
+++ opcodes/Parser.lua
@@ -1,5 +1,9 @@
--- THKY, if you see this, change of plans! Instead of 4 bits for instructions and 4 bits for datavalue1 I've decided that we only really need 3 bits for datavalue1. This allows us to now have about 31 instructions, and 7 registers! Much more balanced then 15 instructions and 15 registers.
 local Parser = {}
+local Operator = require("Operator")
+
+function Parser.Init(Registers)
+	Operator.Init(Registers)
+end
 
 function Parser.ParsePair(byte1, byte2)
 	

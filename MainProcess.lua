a8d774d061471023b1365f26b79e4ef8b8d89abf
lego man <rajawilliams24@gmail.com>
2020-07-04 16:37:22 -0700

    Commit opcode changes
    I added some examples for you to expand upon THKY

--- MainProcess.lua
+++ MainProcess.lua
@@ -4,9 +4,9 @@ local Parser = require("opcodes/Parser")
 local file_ROM = arg[1] or "rom.bin"
 
 local ROM = Memory.FromFile(file_ROM, true)
-local RAM = Memory.new(65535) -- we may need more addresses eventually
-local Registers = Memory.new(7)
+local RAM = Memory.new(65536)
+local Registers = Memory.new(8) 
 
-
-
--- TODO: opcodes
+Parser.Init(Registers, ROM)
+Parser.Loop()
+ 
--- opcodes/Operator.lua
+++ opcodes/Operator.lua
@@ -8,8 +8,10 @@ local function LoadToRegister()
 	
 end
 
-local function And(a,b)
-	return bit32.band(a,b) -- There can't really be returns for these machine code instructions. We would have to set to a register or modify an existing register. I think instead of taking two values and bitwise and them, we should take register at address 0x0 and register at address 0x1 and bitwise and those instead. Then the output could easily go into 0x2 or just 0x0 again.
+local function jmp(parser) -- jump to address in rom
+	local adr1 = parser.Read()
+	print(adr1) -- temporary for debugging
+	parser.Set(adr1 - 1) -- make it the next address to be read
 end
 
 local function Or(a,b)
@@ -20,6 +22,7 @@ local function Not(x)
 	return bit32.bnot(x)
 end
 
+Operators[0x41] = jmp 
 Operators[0x2] = And
 Operators[0x3] = Or
 Operators[0x4] = Not

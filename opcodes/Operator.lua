a8552a4da30929bb2fbd98d0dc55a74be2aa38e5
TTHHKKYY <TheHolyBreadman@users.noreply.github.com>
2020-07-03 23:02:11 -0600

    Added new commands.

--- opcodes/Operator.lua
+++ opcodes/Operator.lua
@@ -1,1 +1,19 @@
 local Operators = {}
+
+local function And(a,b)
+	return bit32.band(a,b)
+end
+
+local function Or(a,b)
+	return bit32.bor(a,b)
+end
+
+local function Not(x)
+	return bit32.bnot(x)
+end
+
+Operators[0x0] = And
+Operators[0x1] = Or
+Operators[0x2] = Not
+
+return Operators
